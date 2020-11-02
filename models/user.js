68b90ba8b514f5df74b8f9322eff3a9cc8be8de2
ajiYudhanto <aji.yudhanto@gmail.com>
2020-11-03 00:08:54 +0700

    done: - configurating postgres - validate user

---
+++ models/user.js
@@ -0,0 +1,44 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class User extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  };
+  User.init({
+    username: {
+      type: DataTypes.STRING,
+      validate: {
+        notEmpty: true,
+        notNull: true
+      }
+    },
+    email: {
+      type: DataTypes.STRING,
+      validate: {
+        isEmail: true,
+        notEmpty: true,
+        notNull: true
+      }
+    },
+    password: {
+      type: DataTypes.STRING,
+      validate: {
+        notEmpty: true,
+        notNull: true
+      }
+    }
+  }, {
+    sequelize,
+    modelName: 'User',
+  });
+  return User;
+};
